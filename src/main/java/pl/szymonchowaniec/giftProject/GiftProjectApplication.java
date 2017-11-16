caa231c4795397519b7edecb050bc2d3d3fe9c26
Szymon Chowaniec <szymonchowaniec@gmail.com>
2017-11-16 16:40:24 +0100

    Initial commit

---
+++ src/main/java/pl/szymonchowaniec/giftProject/GiftProjectApplication.java
@@ -0,0 +1,12 @@
+package pl.szymonchowaniec.giftProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftProjectApplication.class, args);
+	}
+}
