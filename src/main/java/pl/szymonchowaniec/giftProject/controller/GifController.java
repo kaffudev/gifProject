7a98d474211dda1dd7b5dafc524e3195fcb0ce08
kaffudev <kaffudev@gmail.com>
2017-11-17 12:09:08 +0100

    second commitadad

--- src/main/java/pl/szymonchowaniec/giftProject/controller/GifController.java
+++ src/main/java/pl/szymonchowaniec/giftProject/controller/GifController.java
@@ -1,4 +1,31 @@
 package pl.szymonchowaniec.giftProject.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import pl.szymonchowaniec.giftProject.repository.GifRepository;
+
+@Controller
 public class GifController {
+
+
+
+    @Autowired
+    GifRepository gifRepository;
+
+
+    @GetMapping("/")
+    public String showGifs(@RequestParam(required = false)String q, ModelMap modelMap){
+
+        if (q != null){
+
+        }
+
+        modelMap.addAttribute("gifs", gifRepository.getAllGifs());
+        return "home";
+    }
+
+
 }
--- src/main/java/pl/szymonchowaniec/giftProject/model/Gif.java
+++ src/main/java/pl/szymonchowaniec/giftProject/model/Gif.java
@@ -1,4 +1,29 @@
 package pl.szymonchowaniec.giftProject.model;
 
 public class Gif {
+
+    private String name;
+
+    private boolean favorite;
+
+    public Gif(String name, boolean favorite) {
+        this.name = name;
+        this.favorite = favorite;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isFavorite() {
+        return favorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        this.favorite = favorite;
+    }
 }
